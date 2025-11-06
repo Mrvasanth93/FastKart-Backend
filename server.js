3317e74eec59f240d075402e557aa5c876ee7a43
Mrvasanth93 <mrvasanth93@gmail.com>
2025-11-06 14:04:18 +0530

    update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const app = express()
 
 app.use(express.json())
 
-app.use(cors({origin:'http://localhost:5173',credentials:true}))
+app.use(cors({origin:'https://fastkart-coral.vercel.app',credentials:true}))
 
 connectDb();
 
